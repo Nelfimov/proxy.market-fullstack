6d73a5d6fed85b90b29c750b5707a73baff596f2
Nelfimov <nelfimov@mail.ru>
2023-03-29 20:17:14 +0300

    added transition div

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,19 +1,23 @@
 import { useReducer } from 'react';
 import { Container } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import { Footer, NavBar } from '../components';
 import { reducer } from '../reducer';
 import { State } from '../types';
 
 export function App() {
   const initialState: State = { country: '', time: '', type: '', total: 0 };
-
   const [state, dispatch] = useReducer(reducer, initialState);
+  const navigation = useNavigation();
 
   return (
     <>
       <NavBar />
       <Container className='mt-4' style={{ minHeight: '77.5vh' }}>
+        <div
+          id='transition'
+          className={navigation.state === 'loading' ? 'loading' : ''}
+        />
         <Outlet context={[state, dispatch]} />
       </Container>
       <Footer />
