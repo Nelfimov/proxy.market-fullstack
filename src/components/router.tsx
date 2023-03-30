5431db53a71b5a5f2f424da0843a3a53aea6f9e3
Nelfimov <nelfimov@mail.ru>
2023-03-30 16:26:56 +0300

    added basename for deployment

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,27 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { App, Home, Product, Contacts, Cart } from '../pages';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: '/buy-proxie',
-        element: <Product />,
-      },
-      {
-        path: '/contacts',
-        element: <Contacts />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: '/buy-proxie',
+          element: <Product />,
+        },
+        {
+          path: '/contacts',
+          element: <Contacts />,
+        },
+        {
+          path: '/cart',
+          element: <Cart />,
+        },
+      ],
+    },
+  ],
+  { basename: '/proxy.market-fullstack/' }
+);
