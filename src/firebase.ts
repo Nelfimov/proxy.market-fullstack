dd8d5f73d1d04516822c66f72d82d9b845975d1e
Nelfimov <nelfimov@mail.ru>
2023-03-30 12:00:27 +0300

    added firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCYn_w6wgnbkFFFIHsNBXd_35SWbwkNbFM',
+  authDomain: 'proxymarket-backend.firebaseapp.com',
+  projectId: 'proxymarket-backend',
+  storageBucket: 'proxymarket-backend.appspot.com',
+  messagingSenderId: '528006668831',
+  appId: '1:528006668831:web:a235498cfd0ecd60d5eb2a',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
